6d437c3e902327410e643bc71e39a5446c13147b
sid <sidsurakanti1@gmail.com>
2025-04-20 20:01:39 -0400

    chore: remove useless about nav link

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -13,7 +13,7 @@ export function Nav() {
       <nav className="flex justify-center h-24 p-2">
         <ul className="p-1 px-3 h-fit my-auto rounded-3xl flex items-center bg-gradient-to-r from-secondary/70 to-secondary text-primary hover:from-primary/70 hover:to-primary hover:text-secondary">
           <li>
-            <Link href="/">about</Link>
+{/*             <Link href="/">about</Link> */}
           </li>
         </ul>
       </nav>
